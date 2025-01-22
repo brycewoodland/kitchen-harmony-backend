966a53097c37a3c08cf15f04e03acb3583c1dd41
brycewoodland <bwoodlan@byui.edu>
2025-01-22 12:16:58 -0700

    createUser controller function.

---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+const User = require('../models/userModel');
+
+/**
+ * @function createUser
+ * @description Creates a new user in the database.
+ * @param {Object} req - Express request object containing user data in the body.
+ * @param {Object} res - Express response object used to send back the created user or an error message.
+ * @returns {void}
+ */
+const createUser = async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+module.exports = {
+    createUser
+}
