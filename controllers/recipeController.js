446d68f218a849b28f89380a57af2eec92e239ef
brycewoodland <bwoodlan@byui.edu>
2025-01-30 21:01:24 -0700

    Completed Recipe Controller.

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -35,7 +35,77 @@ const getRecipeById = async (req, res) => {
     }
 };
 
+/**
+ * @function createRecipe
+ * @description Posts a new recipe.
+ * @param {Object} req - Express request object containing user data in the body.
+ * @param {Object} res - Express response object used to send back the created user or an error message.
+ * @returns {void}
+ */
+const createRecipe = async (req, res) => {
+    const recipe = new Recipe({
+        title: req.body.title,
+        description: req.body.description,
+        ingredients: req.body.ingredients,
+        instructions: req.body.instructions,
+        createdAt: req.body.createdAt,
+        author: req.body.author,
+        isPublic: req.body.isPublic,
+        tags: req.body.tags,
+        imageUrl: req.body.imageUrl,
+        userId: req.body.userId
+    });
+
+    try {
+        const newRecipe = await recipe.save();
+        res.status(201).json({ message: newRecipe._id });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+/**
+ * @function updateRecipe
+ * @description Gets a recipe by its id and updates its information.
+ * @param {Object} req - Express request object containing user data in the body.
+ * @param {Object} res - Express response object used to send back the created user or an error message.
+ * @returns {void}
+ */
+const updateRecipe = async (req, res) => {
+    try {
+        const recipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!recipe) {
+            return res.status(404).json({ message: 'Recipe not found.'});
+        }
+        res.json(recipe);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+/**
+ * @function deleteRecipe
+ * @description Gets a recipe by its id and deletes its information.
+ * @param {Object} req - Express request object containing user data in the body.
+ * @param {Object} res - Express response object used to send back the created user or an error message.
+ * @returns {void}
+ */
+const deleteRecipe = async (req, res) => {
+    try {
+        const recipe = await Recipe.findByIdAndDelete(req.params.id);
+        if (!recipe) {
+            return res.status(404).json({ message: 'Recipe not found.'});
+        }
+        res.send(recipe);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
 module.exports = {
     getAllRecipes,
-    getRecipeById
+    getRecipeById,
+    createRecipe,
+    updateRecipe,
+    deleteRecipe
 }
